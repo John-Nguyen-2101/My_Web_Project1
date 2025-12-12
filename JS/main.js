05383f81c44c68ac6aaf4a55905d4a61e9e9197b
Thanh Lich Nguyen <johnthanhlich01@gmail.com>
2025-12-12 11:41:25 +1100

    update file

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // remove loader
-    const loader = document.getElementById("loader");
-    if (loader) loader.remove();
+  const loader = document.getElementById("loader");
+  if (loader) loader.remove();
 
-    // show showreel
-    const showreel = document.getElementById("showreel");
-    if (showreel) showreel.hidden = false;
+  const showreel = document.getElementById("showreel");
+  if (showreel) showreel.hidden = false;
 
-    const thumb = document.getElementById("videoThumb");
-    const iframe = document.getElementById("videoFrame");
+  const thumb = document.getElementById("videoThumb");
+  const iframe = document.getElementById("videoFrame");
 
-    thumb.addEventListener("click", () => {
-      iframe.src =
-        "https://www.youtube.com/embed/vkbPTWo5y5U?autoplay=1&rel=0";
-      thumb.style.display = "none";
-      iframe.style.display = "block";
-    });
+  thumb.addEventListener("click", () => {
+    iframe.src =
+      "https://www.youtube.com/embed/vkbPTWo5y5U?autoplay=1&rel=0";
+    iframe.style.opacity = "1";
+    iframe.style.pointerEvents = "auto";
+    thumb.style.opacity = "0";
+    thumb.style.pointerEvents = "none";
   });
+});
